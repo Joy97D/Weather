04627a3da1c7816e627383fe7ddd9195a2938852
Srinjoy Das <srinjoy98x@gmail.com>
2024-01-23 23:08:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import React from 'react'
 import { useState,useEffect } from "react";
 function App() {
-  const [city,setcity]=useState()
+  const [city,setcity]=useState('')
   const [lat,setlat]=useState()
   const [lon,setlon]=useState()
   const [nocity,setnocity]=useState(false)
   const [result,setresult]=useState()
    const  handleclick=async()=>{
-      const user= await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=451a5017cc4ccbbfd9191226a6e00359`)
+      const user= await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.REACT_APP_API_KEY}`)
       const geo = await user.json();
       if(geo[0]){
       setlat(geo[0].lat)
@@ -19,7 +19,7 @@ function App() {
       {setnocity(true)}
    }
    const  getweather =async()=>{
-    const weather= await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=451a5017cc4ccbbfd9191226a6e00359`)
+    const weather= await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`)
     const w=await weather.json()
     setresult(`Temperature at ${city} is ${(w.main.temp-273.15).toFixed(2)}C and Weather Condition is ${w.weather[0].description}`)
    }
