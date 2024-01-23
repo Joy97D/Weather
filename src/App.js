147a80d48ffe23a898a237eee2a321b57b952588
Srinjoy Das <srinjoy98x@gmail.com>
2024-01-23 22:45:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
+import { useState,useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  const [city,setcity]=useState()
+  const [lat,setlat]=useState()
+  const [lon,setlon]=useState()
+  const [nocity,setnocity]=useState(false)
+  const [result,setresult]=useState()
+   const  handleclick=async()=>{
+      const user= await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=451a5017cc4ccbbfd9191226a6e00359`)
+      const geo = await user.json();
+      if(geo[0]){
+      setlat(geo[0].lat)
+      setlon(geo[0].lon)
+      setnocity(false)
+      }
+      else
+      {setnocity(true)}
+   }
+   const  getweather =async()=>{
+    const weather= await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=451a5017cc4ccbbfd9191226a6e00359`)
+    const w=await weather.json()
+    setresult(`Temperature at ${city} is ${(w.main.temp-273.15).toFixed(2)}C and Weather Condition is ${w.weather[0].description}`)
+   }
+   useEffect(()=>{
+    if(lat!==undefined && lon!==undefined)
+    {
+      getweather()
+    }
+   },[lat,lon])
+    return (
+      <div className='Main'>
+        <h1>Weather App</h1>
+        <div className='App'>
+      <input defaultValue={city} onChange={(e)=>setcity(e.target.value)} placeholder='Enter City' size={30} height={100}></input>
+      <br/>
+      <button onClick={handleclick} disabled={city===(undefined||'')? 1:0}>Get Weather Report</button>
+      <div className='Disp'>
+        { nocity &&
+           <>No City Found</>
+        }
+        { !nocity &&
+        <>{result}</>
+        }
+        </div>
+        </div>
+      </div>
+    );
+  }
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import Extra from './Extra';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <Extra/> */}
     <App />
   </React.StrictMode>
 );
